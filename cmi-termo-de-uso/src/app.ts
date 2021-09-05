eace0b9caa93582a24f49efe944f352a4ad365b1
AysllaGomes <ayslla.gomes@outlook.com>
2021-09-05 18:38:40 -0300

    [UPT] [TASK-????] Configurando bodyParser.json, middlewareForLog, mergePatchBodyParser e adicionando logs

--- cmi-termo-de-uso/src/app.ts
+++ cmi-termo-de-uso/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { logger } from "./util/logger";
 import { DocsApi } from "./routes/docs.api";
 import { MainApi } from "./routes/main.api";
 import { ErrorApi } from "./routes/error.api";
+import { environment } from "./config/environment";
 import { handleError } from "./util/error.handler";
 import { TermoDeUsoApi } from "./routes/TermoDeUso.api";
-import { verificaConexaoMongoMiddleware } from "./util/middleware";
+import { mergePatchBodyParser, middlewareForLog, verificaConexaoMongoMiddleware } from "./util/middleware";
 
 const getApiControllers = (): (ErrorApi | MainApi | DocsApi | TermoDeUsoApi)[] => [
   new ErrorApi(), new MainApi(), new DocsApi(), new TermoDeUsoApi(),
@@ -23,6 +25,14 @@ rotasPostVerificadas.forEach((nomeDaRota: string) => {
   // @ts-ignore
   app.post(nomeDaRota, verificaConexaoMongoMiddleware);
 });
+
+app.locals.name = environment.app.name;
+app.locals.version = environment.app.version;
+
+app.use(bodyParser.json());
+
+app.use(middlewareForLog);
+app.use(mergePatchBodyParser);
 
 logger.info("Configurando Rotas");
 for (let i = 0; i < getApiControllers().length; i++) {
