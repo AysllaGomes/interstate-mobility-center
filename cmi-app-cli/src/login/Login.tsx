9c62dfb0c0b542a86b874e3f6ff32d491373e944
EttoreDezan <dezan3@outlook.com>
2021-09-07 19:10:53 -0300

    Alterações no Login e Register.

--- cmi-app-cli/src/login/Login.tsx
+++ cmi-app-cli/src/login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Alert, SafeAreaView, View} from "react-native";
 import {Button, Card, TextInput} from "react-native-paper";
 import {loginStyle} from "./login.style";
@@ -12,14 +12,26 @@ const Login = (props: LoginScreenProps) => {
     const logar = () => props.navigation.navigate("Home")
     const register = () => props.navigation.navigate("Register")
 
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+
     return (
         <SafeAreaView style={loginStyle.content}>
             <View style={loginStyle.view}>
                 <Card>
                     <Card.Title title="Mobility Center" titleStyle={loginStyle.cardTitle}></Card.Title>
                     <Card.Content>
-                        <TextInput label="User" keyboardType="email-address"></TextInput>
-                        <TextInput label="Password" secureTextEntry={true}></TextInput>
+                        <TextInput
+                            placeholder="Email"
+                            label="User"
+                            keyboardType="email-address"
+                            onChangeText={value => setEmail(value)}
+                        />
+                        <TextInput
+                            label="Password"
+                            secureTextEntry={true}
+                            onChangeText={value => setPassword(value)}
+                        />
                         <Button uppercase={false} style={loginStyle.cardButton}>Esqueceu email/password?</Button>
                         <Button onPress={logar} mode="contained" style={loginStyle.cardButton}>Login</Button>
                         <Button onPress={register} style={loginStyle.cardButton}>Register</Button>
--- cmi-app-cli/src/register/register.screen.tsx
+++ cmi-app-cli/src/register/register.screen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {SafeAreaView, ScrollView, View} from "react-native";
 import {Appbar, Button, TextInput} from "react-native-paper";
 import {registerStyle} from "./register.style";
@@ -8,21 +8,61 @@ interface LoginScreenProps {
     navigation: any;
 }
 
+
+
 export const RegisterScreen = (props: LoginScreenProps) => {
     const logar = () => props.navigation.navigate("Home")
-    return (
-        <SafeAreaView>
-            <ScrollView>
-                    <HeaderComponent title="Register" />
+
+    const validaPassword = (password, confPassword) => password === confPassword
+
+    const [nome, setNome] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [dataNascimento, setDataNascimento] = useState(null)
+    const [phoneNumber, setPhoneNumber] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confPassword, setConfPassword] = useState(null)
+    console.log(email)
+    console.log(validaPassword(password, confPassword))
+    return(
+         <SafeAreaView>
+             <ScrollView>
+                       <HeaderComponent title="Register" />
                     <View style={registerStyle.content}>
-                        <TextInput label="Name" />
-                        <TextInput label="Email" keyboardType="email-address" />
-                        <TextInput label="Password" secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline" color={registerStyle.icon.color} />} />
-                        <TextInput label="Confirm password" secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline" color={registerStyle.icon.color} />}/>
-                        <TextInput label="Phone number" keyboardType="phone-pad" />
+                          <TextInput
+                          label="Nome Completo"
+                          placeholder="Nome"
+                          onChangeText={value => setNome(value)}
+                          />
+                        <TextInput
+                            label="E-mail"
+                            onChangeText={value => setEmail(value)}
+                            keyboardType="email-address"
+                        />
+                        <TextInput
+                            onChangeText={value => setPhoneNumber(value)}
+                            label="Phone number"
+                            keyboardType="phone-pad"
+                        />
+                        <TextInput
+                            onChangeText={value => setDataNascimento(value)}
+                            placeholder="dd/mm/aaa"
+                            label="Data nascimento"
+                            keyboardType="numeric" />
+                        <TextInput
+
+                            onChangeText={value => setPassword(value)}
+                            placeholder="Password" secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline"
+                            color={registerStyle.icon.color} />}
+                        />
+                        <TextInput
+                            onChangeText={value => setConfPassword(value)}
+                            label="Confirm password"
+                            secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline"
+                            color={registerStyle.icon.color} />}
+                        />
                         <Button onPress={logar} mode="contained" style={registerStyle.button}>Register</Button>
                     </View>
-            </ScrollView>
+             </ScrollView>
         </SafeAreaView>
-    )
+    );
 }
