c358cb0bae43ea9c449bc631dac0268dc7c1b467
AysllaGomes <ayslla.gomes@outlook.com>
2021-09-21 22:05:31 -0300

    [ADD] Criação do MS Contrato

---
+++ cmi-contrato/spec/test/unit/Database/ConectarRetornandoComErro.ut.test.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import mongoose from "mongoose";
+import Database from "../../../../src/config/database";
+import { jaegerTracer } from "../utils/funcoes/JaegerTracer";
+import { environment } from "../../../../src/config/environment";
+import { ConexaoMongoEnum } from "../../../../src/model/enums/ConexaoMongo.enum";
+
+beforeAll(() => jaegerTracer);
+
+jest.mock("mongoose", () => ({
+  connect: jest.fn().mockResolvedValue(true),
+}));
+
+describe("Teste do serviço que se conecta ao bd", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("Teste MOCK do método conectar - retornando erro", async () => {
+    environment.db.uri = "";
+    const result = await Database.conectar();
+
+    expect(result).toBeDefined();
+    expect(result).toBe(ConexaoMongoEnum.disconnected);
+  });
+});
---
+++ cmi-contrato/src/model/VariaveisDeAmbiente.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface INocksMS {
+    MSAutentica: boolean,
+    MSPassageiro: boolean,
+    MSViagem: boolean,
+    cenarioDaViagem : {
+        cenario : number,
+        cenariosDisponiveis : {
+            1 : string,
+            2 : string,
+        }
+    }
+}
+
+export interface IVariaveisDeAmbiente extends Document {
+  _id: string;
+  codigoVariavel : string;
+  habilitaNock?: INocksMS;
+  sequencialNumeroVersaoTermoDeUso: number;
+}
+
+const VariaveisDeAmbienteSchema: Schema = new Schema({
+  codigoVariavel: {
+    type: String,
+    required: true,
+  },
+  habilitaNock: {
+    type: Object,
+    required: false,
+  },
+  sequencialNumeroVersaoTermoDeUso: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default mongoose.model<IVariaveisDeAmbiente>("VariaveisDeAmbiente", VariaveisDeAmbienteSchema, "variaveisDeAmbiente");
