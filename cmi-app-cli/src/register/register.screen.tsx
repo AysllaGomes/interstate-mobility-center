ae3acb87ea6043f44e49ebbffd00c10ec927b343
EttoreDezan <dezan3@outlook.com>
2021-09-28 16:24:14 -0300

    Conecção CMI cadastro de usuário! Criação PopUp Erro tela de registro de usuário!

--- cmi-app-cli/src/register/register.screen.tsx
+++ cmi-app-cli/src/register/register.screen.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button, TextInput} from "react-native-paper";
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import {SafeAreaView, ScrollView, Text, View, TouchableOpacity, ToastAndroid} from "react-native";
 import { registerStyle } from "./register.style";
 import { HeaderComponent } from "../components/header/header.component";
 import firebase from "../firebase/firebaseconfig";
 import createUser from "./register.service";
 import { Formik } from 'formik';
 import {registerForm} from "./register.form";
+import { Popup, Root, Toast } from 'popup-ui'
 
 interface LoginScreenProps {
     navigation: any, // @todo nunca aceitável
@@ -27,12 +28,13 @@ export const RegisterScreen = (props: LoginScreenProps) => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then((userCredential) => {
                 // Signed in
-                return userCredential.user;
+                userCredential.user;
                 // ...
             })
             .catch((error) => {
                 var errorCode = error.code;
                 var errorMessage = error.message;
+                return errorCode;
             });
 
         return null;
@@ -49,121 +51,130 @@ export const RegisterScreen = (props: LoginScreenProps) => {
     //     return errorPassword;
     // };
 
-
     const registerUser = async (values) => {
-        let user = RegisterWithFirebase(values.email, values.password)
+        let errorFirebase = RegisterWithFirebase(values.email, values.password)
 
-        if (user == null) {
+        if (errorFirebase != null) {
             return console.log("Error with Firebase Register")
         }
-        if (createUser(values) != null) {
-            return console.log("Error with Mongo Register")
-        }else{
-            props.navigation.navigate("Home")
+
+        let errorMongo = await createUser(values)
+
+        console.log(errorMongo)
+
+        if (errorMongo.status != 200) {
+
+            Toast.show({
+                title: 'Houve um problema!',
+                text: errorMongo.data.message,
+                color: '#e74c3c'
+            })
+            return;
         }
+            props.navigation.navigate("Home")
     }
 
         return (
-            <SafeAreaView>
-                <ScrollView>
+            <Root>
+                <SafeAreaView>
+                    <ScrollView>
                     <HeaderComponent title="Register" navigation={props.navigation} page="Login"/>
                     <Formik initialValues={{name: '', email: '', phoneNumber: '', birthDate: '', password: '', ConfPassword: ''}}
-                            onSubmit={values => registerUser(values)}
-                            validationSchema={registerForm}>
-                        {({handleSubmit, handleChange, touched, setFieldTouched, handleBlur, errors, values}) => (
+                                onSubmit={values => registerUser(values)}
+                                validationSchema={registerForm}>
+                            {({handleSubmit, handleChange, touched, setFieldTouched, handleBlur, errors, values}) => (
 
-                        <View style={registerStyle.content}>
-                                <>
-                                <TextInput
-                                        label="Nome Completo"
-                                        placeholder="Nome"
-                                        onChangeText={handleChange('name')}
-                                        onFocus={() => setFieldTouched('name')}
-                                        onBlur={handleBlur('name')}
-                                        value={values.name}
-                                />
-                                {
-                                    touched.name && errors.name ? <Text style={{color: "red" }}>
-                                        {errors.name}
-                                    </Text> : null
-                                }
-
-                                <TextInput
-                                    placeholder="Email"
-                                    label="User"
-                                    keyboardType="email-address"
-                                    onChangeText={handleChange('email')}
-                                    onFocus={() => setFieldTouched('email')}
-                                    onBlur={handleBlur('email')}
-                                    value={values.email}
-                                />
-                                {
-                                    touched.email && errors.email ? <Text style={{color: "red" }}>
-                                        {errors.email}
-                                    </Text> : null
-                                }
-
-                                <TextInput
-                                        label="Phone number"
-                                        keyboardType="phone-pad"
-                                        onChangeText={handleChange('phoneNumber')}
-                                        onFocus={() => setFieldTouched('phoneNumber')}
-                                        onBlur={handleBlur('phoneNumber')}
-                                        value={values.phoneNumber}
+                            <View style={registerStyle.content}>
+                                    <TextInput
+                                            label="Nome Completo"
+                                            placeholder="Nome"
+                                            onChangeText={handleChange('name')}
+                                            onFocus={() => setFieldTouched('name')}
+                                            onBlur={handleBlur('name')}
+                                            value={values.name}
                                     />
-                                {
-                                    touched.phoneNumber && errors.phoneNumber ? <Text style={{color: "red" }}>
-                                        {errors.phoneNumber}
-                                    </Text> : null
-                                }
-                                   <TextInput
-                                        placeholder="dd/mm/aaa"
-                                        label="Data nascimento"
-                                        keyboardType="numeric"
-                                        onChangeText={handleChange('birthDate')}
-                                        onFocus={() => setFieldTouched('birthDate')}
-                                        onBlur={handleBlur('birthDate')}
-                                        value={values.birthDate}
-                                   />
-                                {
-                                    touched.birthDate && errors.birthDate ? <Text style={{color: "red" }}>
-                                        {errors.birthDate}
-                                    </Text> : null
-                                }
-                                <TextInput
-                                        placeholder="Password" secureTextEntry={true}
-                                        right={<TextInput.Icon name="eye-off-outline"
-                                        color={registerStyle.icon.color}/>}
-                                        onChangeText={handleChange('password')}
-                                        onFocus={() => setFieldTouched('password')}
-                                        onBlur={handleBlur('password')}
-                                        value={values.password}
-                                />
-                                {
-                                    touched.password && errors.password ? <Text style={{color: "red" }}>
-                                        {errors.password}
-                                    </Text> : null
-                                }
-                                <TextInput
-                                        label="Confirm password"
-                                        secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline"
-                                        color={registerStyle.icon.color}/>}
-                                        onChangeText={handleChange('ConfPassword')}
-                                        onFocus={() => setFieldTouched('ConfPassword')}
-                                        onBlur={handleBlur('ConfPassword')}
-                                        value={values.ConfPassword}
-                                />
-                                {
-                                    touched.ConfPassword && errors.ConfPassword ? <Text style={{color: "red" }}>
-                                        {errors.ConfPassword}
-                                    </Text> : null
-                                }
-                                <Button onPress={handleSubmit}  mode="contained" style={registerStyle.button}>Register</Button>
-                                </>
-                            </View>
-                        )}
-                    </Formik>
-                </ScrollView>
-            </SafeAreaView>
+                                    {
+                                        touched.name && errors.name ? <Text style={{color: "red" }}>
+                                            {errors.name}
+                                        </Text> : null
+                                    }
+
+                                    <TextInput
+                                        placeholder="Email"
+                                        label="User"
+                                        keyboardType="email-address"
+                                        onChangeText={handleChange('email')}
+                                        onFocus={() => setFieldTouched('email')}
+                                        onBlur={handleBlur('email')}
+                                        value={values.email}
+                                    />
+                                    {
+                                        touched.email && errors.email ? <Text style={{color: "red" }}>
+                                            {errors.email}
+                                        </Text> : null
+                                    }
+
+                                    <TextInput
+                                            label="Phone number"
+                                            keyboardType="phone-pad"
+                                            onChangeText={handleChange('phoneNumber')}
+                                            onFocus={() => setFieldTouched('phoneNumber')}
+                                            onBlur={handleBlur('phoneNumber')}
+                                            value={values.phoneNumber}
+                                        />
+                                    {
+                                        touched.phoneNumber && errors.phoneNumber ? <Text style={{color: "red" }}>
+                                            {errors.phoneNumber}
+                                        </Text> : null
+                                    }
+                                       <TextInput
+                                            placeholder="dd/mm/aaa"
+                                            label="Data nascimento"
+                                            keyboardType="numeric"
+                                            onChangeText={handleChange('birthDate')}
+                                            onFocus={() => setFieldTouched('birthDate')}
+                                            onBlur={handleBlur('birthDate')}
+                                            value={values.birthDate}
+                                       />
+                                    {
+                                        touched.birthDate && errors.birthDate ? <Text style={{color: "red" }}>
+                                            {errors.birthDate}
+                                        </Text> : null
+                                    }
+                                    <TextInput
+                                            placeholder="Password" secureTextEntry={true}
+                                            right={<TextInput.Icon name="eye-off-outline"
+                                            color={registerStyle.icon.color}/>}
+                                            onChangeText={handleChange('password')}
+                                            onFocus={() => setFieldTouched('password')}
+                                            onBlur={handleBlur('password')}
+                                            value={values.password}
+                                    />
+                                    {
+                                        touched.password && errors.password ? <Text style={{color: "red" }}>
+                                            {errors.password}
+                                        </Text> : null
+                                    }
+                                    <TextInput
+                                            label="Confirm password"
+                                            secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline"
+                                            color={registerStyle.icon.color}/>}
+                                            onChangeText={handleChange('ConfPassword')}
+                                            onFocus={() => setFieldTouched('ConfPassword')}
+                                            onBlur={handleBlur('ConfPassword')}
+                                            value={values.ConfPassword}
+                                    />
+                                    {
+                                        touched.ConfPassword && errors.ConfPassword ? <Text style={{color: "red" }}>
+                                            {errors.ConfPassword}
+                                        </Text> : null
+                                    }
+                                    <Button onPress={handleSubmit}  mode="contained" style={registerStyle.button}>Register</Button>
+                                </View>
+                            )}
+                        </Formik>
+                    </ScrollView>
+                </SafeAreaView>
+            </Root>
         );
     }
--- cmi-app-cli/src/register/register.service.ts
+++ cmi-app-cli/src/register/register.service.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
-const createUser = (values) => {
-    console.log(values)
-        axios.post('http://localhost:3000/usuario/cadastro', {"nome": values.name, "email": values.email, "dataDeNascimento": values.birthDate, "numeroTelefoneCelular": values.phoneNumber}).then(response => {
-            console.log(response.data)
-        }).catch((error) => {
-            console.log('Error retrieving data')
-            console.log(error)
-            return error;
-        })
-    return null;
+const createUser = async (values) => {
+    const urlBase = "http://192.168.0.107:3000/"
+    try {
+        return await axios.post(urlBase+'usuario/cadastro', {"nome": values.name, "email": values.email, "dataDeNascimento": values.birthDate, "numeroTelefoneCelular": values.phoneNumber})
+    } catch (error) {
+        return error.response
+    }
 }
 export default createUser;
