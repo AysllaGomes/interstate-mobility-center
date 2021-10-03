5295a37c355b88ec2036522658c9f8c6f227c94f
EttoreDezan <dezan3@outlook.com>
2021-10-03 08:55:10 -0300

    Date picker adicionado a Home

--- cmi-app-cli/src/home/index.tsx
+++ cmi-app-cli/src/home/index.tsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
-import {SafeAreaView, View, FlatList, TouchableOpacity, StatusBar, Text, Image} from "react-native";
+import {SafeAreaView, View, FlatList, TouchableOpacity, StatusBar, Text, Image, Button} from "react-native";
 import { TextInput } from "react-native-paper";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import results from "./results";
 import {indexStyle} from "./index.style";
-
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 interface HomeScreenProps {
     navigation: any,
     page: null
@@ -28,6 +28,8 @@ const HomeScreen = (props: HomeScreenProps) => {
 
     const [searchText, setSearchText] = useState('');
     const [list, setList] = useState(results)
+    const [dates, setDates] = useState({arrival: '', departure: ''})
+
 
     useEffect(() =>{
         if(searchText === '') {
@@ -39,7 +41,60 @@ const HomeScreen = (props: HomeScreenProps) => {
         }
     }, [searchText]);
 
-    const handleOrderClick = () => {};
+    useEffect(() =>{
+        if(dates.departure === '' && dates.arrival === '') {
+            setList(results);
+        }else {
+            setList(
+                results.filter((pacote) => pacote.dateArrival == dates.arrival && pacote.dateDeparture == dates.departure)
+            );
+        }
+    }, [dates]);
+
+    const [isDatePickerVisible, setDatePickerVisibility] = useState({departure: false, arrival: false});
+
+    const showDatePickerDeparture = () => {
+        setDatePickerVisibility({...isDatePickerVisible, departure: true, });
+    }
+    const hideDatePickerDeparture = () => {
+        setDatePickerVisibility({...isDatePickerVisible, departure: false});
+    }
+
+    const handleConfirmDeparture = (date) => {
+         const departureDate = date.getDate().toString().concat("/").concat((date.getUTCMonth()+1).toString()).concat("/").concat(date.getFullYear().toString())
+         hideDatePickerDeparture()
+
+        setDates({...dates, departure: departureDate})
+    }
+    const showDatePickerArrival = () => {
+        setDatePickerVisibility({...isDatePickerVisible, arrival: true, });
+
+
+    }
+    const hideDatePickerArrival = () => {
+        setDatePickerVisibility({...isDatePickerVisible, arrival: false, });
+    }
+
+
+    const handleConfirmArrival = (date) => {
+        const dateArrival = date.getDate().toString().concat("/").concat((date.getUTCMonth()+1).toString()).concat("/").concat(date.getFullYear().toString())
+        hideDatePickerArrival()
+
+        setDates({...dates, arrival: dateArrival})
+    }
+
+    const filterByDate = (dateArrival, dateDeparture) => {
+        return results.filter((pacote) => pacote.dateArrival == dateArrival && pacote.dateDeparture == dateDeparture);
+    }
+
+    console.log('Arrival Date', dates.arrival);
+    console.log('Departure Date', dates.departure);
+    console.log('Datas ', filterByDate(dates.arrival, dates.departure))
+
+    function handleOrderClick() {
+
+    }
+
     return(
         <SafeAreaView>
             <View>
@@ -55,6 +110,23 @@ const HomeScreen = (props: HomeScreenProps) => {
                         color="#888"
                     />
                 </TouchableOpacity>
+                <Button title="Show date partida" onPress={showDatePickerDeparture}>Date</Button>
+
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible.departure}
+                    mode="date"
+                    onConfirm={handleConfirmDeparture}
+                    onCancel={hideDatePickerDeparture}
+
+                />
+                <Button title="Show date Picker Chegada" onPress={showDatePickerArrival}>Data chegada</Button>
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible.arrival}
+                    mode="date"
+                    onConfirm={handleConfirmArrival}
+                    onCancel={hideDatePickerArrival}
+
+                />
             </View>
             <FlatList data={list} renderItem={({item}) => <TravelListItem navigation={props.navigation} data={item}  />}
                       keyExtractor={(item) => item.id}
@@ -64,5 +136,6 @@ const HomeScreen = (props: HomeScreenProps) => {
     );
 
 }
+
 
 export default HomeScreen;
--- cmi-app-cli/src/register/register.service.ts
+++ cmi-app-cli/src/register/register.service.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 const createUser = async (values) => {
     const urlBase = "http://192.168.0.107:3001/"
+    console.log("Chegou aqui")
     try {
-        return await axios.post(urlBase+'usuario/cadastro', {"nome": values.name, "email": values.email, "dataDeNascimento": values.birthDate, "numeroTelefoneCelular": values.phoneNumber.replace(/\D/g,''), "cpf": values.cpf})
+        let err = await axios.post(urlBase+'usuario/cadastro', {"nome": values.name, "email": values.email, "dataDeNascimento": values.birthDate, "numeroTelefoneCelular": values.phoneNumber.replace(/\D/g,''), "cpf": values.cpf})
+        console.log('err', err);
     } catch (error) {
+        console.log('error.response', error);
         return error.response
     }
 }
