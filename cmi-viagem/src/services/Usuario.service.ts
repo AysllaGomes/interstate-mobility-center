ba5bb40a46628b1aec0df9940f50119f72890688
AysllaGomes <ayslla.gomes@outlook.com>
2021-10-04 21:16:13 -0300

    [UPT] Retornando os dados do usuário no parceiro

--- cmi-viagem/src/services/Usuario.service.ts
+++ cmi-viagem/src/services/Usuario.service.ts
@@ -4,8 +4,11 @@ import { environment } from "../config/environment";
 import {
   ErroExterno,
   ERRO_EXTERNO_AO_CONSULTAR_USUARIO,
+  ERRO_EXTERNO_NA_BUSCA_DO_USUARIO_NOS_PARCEIROS,
 } from "../errors/erro.externo";
 import { IUsuario } from "../model/Usuario";
+import { IRealizaCotacao } from "../model/interfaces/RealizaCotacao";
+import { IUsuarioParceiro } from "../model/interfaces/UsuarioParceiro";
 
 export class UsuarioService {
   static async retornaDadosPassageiro(idUsuario: string): Promise<IUsuario> {
@@ -28,6 +31,29 @@ export class UsuarioService {
       `);
 
       throw new ErroExterno(...ERRO_EXTERNO_AO_CONSULTAR_USUARIO).formatMessage(error.message);
+    }
+  }
+
+  static async consultaUsuarioDoParceiro(body: IRealizaCotacao): Promise<IUsuarioParceiro> {
+    try {
+      logger.debug(`Consulta do usuário ${body.idUsuario} nos parceiros...`);
+
+      const result = await axios({
+        method: "get",
+        url: `${environment.app.hostCMIUsuarioParceiro}/parceiro/usuarios/${body.idUsuario}`,
+      });
+
+      return result.data;
+    } catch (error) {
+      logger.error(`
+        ERRO no MS "${environment.app.name}", método "consultaUsuarioDoParceiro".
+        <'ERRO'>
+          message: ERRO ao realizar a consulta do usuario ${body.idUsuario} nos parceiros...
+        Parâmetros da requisição:
+          ID USUÁRIO: ${body.idUsuario}
+      `);
+
+      throw new ErroExterno(...ERRO_EXTERNO_NA_BUSCA_DO_USUARIO_NOS_PARCEIROS).formatMessage(body.idUsuario, error.message);
     }
   }
 }
