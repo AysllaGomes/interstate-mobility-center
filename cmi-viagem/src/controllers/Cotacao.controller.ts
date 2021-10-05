a0cedf85814b2c5216a1fec801378d8e1596ff58
AysllaGomes <ayslla.gomes@outlook.com>
2021-10-04 21:15:23 -0300

    [UPT] Alterando a controller com o retorno esperado.

--- cmi-viagem/src/controllers/Cotacao.controller.ts
+++ cmi-viagem/src/controllers/Cotacao.controller.ts
@@ -1,6 +1,7 @@
 import { setTagSpan, traceable } from "jaeger-tracer-decorator";
 import { CotacaoService } from "../services/Cotacao.service";
 import { IRealizaCotacao } from "../model/interfaces/RealizaCotacao";
+import { IRetornaCotacao } from "../model/interfaces/RetornaCotacao";
 
 @traceable()
 export class CotacaoController {
@@ -10,11 +11,9 @@ export class CotacaoController {
 
   private service: CotacaoService;
 
-  constructor({ service = new CotacaoService() }) {
-    this.service = service;
-  }
+  constructor({ service = new CotacaoService() }) { this.service = service; }
 
-  public async retornaMelhorCotacao(body: IRealizaCotacao): Promise<any> {
+  public async retornaMelhorCotacao(body: IRealizaCotacao): Promise<IRetornaCotacao | undefined> {
     return this.service.retornaMelhorCotacao(body);
   }
 }
